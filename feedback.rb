c52da827b2ec2d88b2d158b35efbf9346aedaac9
Sarah Barnekow <mellamopepe@gmail.com>
2015-01-13 14:30:25 -0600

    able to scrape emails from gmail and create cards with them in ming

--- feedback.rb
+++ feedback.rb
@@ -1,14 +1,41 @@
 require 'gmail'
+require 'net/https'
+require 'time'
+require 'api-auth'
+require 'json'
 
-def open_gmail_connection
-	@gmail = Gmail.connect("<gmail username>", "<gmail password>")
-	p @gmail.inbox.count
+def http_post(url, params, options={})
+  uri = URI.parse(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true 
+  body = params.to_json
+
+  request = Net::HTTP::Post.new(uri.request_uri)
+  request.body = body
+  request['Content-Type'] = 'application/json'
+  request['Content-Length'] = body.bytesize
+
+  ApiAuth.sign!(request, options[:access_key_id], options[:access_secret_key])
+
+  response = http.request(request)
+  card = response.body
+  
+  card 
 end
 
-def close_gmail_connection
-	@gmail.logout
-	p "hi, I am no longer connected"
-end
+gmail = Gmail.connect("<email username>", "<email password>")
+emails = gmail.inbox.find(:unread, :from => 'do-not-reply@thoughtworks.com')
+URL = 'https://sarahh.mingle-api.thoughtworks.com/api/v2/projects/test_project/cards.xml'
+OPTIONS = {:access_key_id => '<access key>', :access_secret_key => '<secret key>'}
 
-open_gmail_connection
-close_gmail_connection
+emails.each do |email|
+	PARAMS = { 
+	  :card => { 
+	    :type => "Feedback", :name => email.subject, :description => email.body
+	    }
+	  }
+
+	http_post(URL, PARAMS, OPTIONS)
+
+	email.read! 
+end
